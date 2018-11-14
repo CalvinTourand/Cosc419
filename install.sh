01177ea744e5d53be3b4ec4f0b0c0135a971828a
CalvinTourand <calvintourand@gmail.com>
2018-11-14 23:19:11 +0000

    Part 4

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo yum install python-pip
+sudo pip install --upgrade pip
+sudo pip install flask
+sudo curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
+sudo yum install -y mod_passenger || sudo yum-config-manager --enable cr && sudo yum install -y mod_passenger
+mkdir /var/www/lab8
+cp -R * /var/www/lab8
+cp myapp.conf /etc/httpd/conf.d/myapp.conf
+service httpd restart
